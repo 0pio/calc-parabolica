e7332ce28c43e8592a9fc0744657bfa929158ca8
pio <arpiochess@gmail.com>
2023-09-16 13:27:23 -0600

    obstaculo y cajas de resultado puestos

--- nueva/calculadora.py
+++ nueva/calculadora.py
@@ -39,80 +39,97 @@ def calculo_tiro():
 
 #Create the main window 
 root = tk.Tk()
-root.geometry('1000x700')
+root.geometry('1920x1080')
 root.title("Ucalc Interfaz")
+root.configure(bg="white")
 
   
 # Create the entry field for the numbers
-entry_m = ttk.Entry(root, width=9  , background="white",)
+entry_m = ttk.Entry(root, width=9  , background="white", font=("Lucida Sans Typewriter", 11))   
 entry_m.pack(pady=5)
-entry_m.place(relx=0.34, rely=0.1)
+entry_m.place(relx=0.24, rely=0.1)
 
-entry_g = ttk.Entry(root, width=9)
+entry_g = ttk.Entry(root, width=9, background="white", font=("Lucida Sans Typewriter", 11))
 entry_g.pack(pady=5)
-entry_g.place(relx=0.34, rely=0.17)
+entry_g.place(relx=0.24, rely=0.17)
 
-entry_k = ttk.Entry(root, width=9)
+entry_k = ttk.Entry(root, width=9, background="white", font=("Lucida Sans Typewriter", 11))
 entry_k.pack(pady=5)
-entry_k.place(relx=0.34, rely=0.24)
+entry_k.place(relx=0.24, rely=0.24)
 
-entry_Ho = ttk.Entry(root, width=9)
+entry_Ho = ttk.Entry(root, width=9, background="white", font=("Lucida Sans Typewriter", 11))
 entry_Ho.pack(pady=5)
-entry_Ho.place(relx=0.34, rely=0.31)
+entry_Ho.place(relx=0.24, rely=0.31)
 
-entry_Hf = ttk.Entry(root, width=9)
+entry_Hf = ttk.Entry(root, width=9, background="white", font=("Lucida Sans Typewriter", 11))
 entry_Hf.pack(pady=5)   
-entry_Hf.place(relx=0.34, rely=0.38)
+entry_Hf.place(relx=0.24, rely=0.38)
 
-entry_L = ttk.Entry(root, width=9)
+entry_L = ttk.Entry(root, width=9, background="white", font=("Lucida Sans Typewriter", 11))
 entry_L.pack(pady=5)
-entry_L.place(relx=0.34, rely=0.45)
+entry_L.place(relx=0.24, rely=0.45)
+
+entry_x = ttk.Entry(root, width=9, background="white", font=("Lucida Sans Typewriter", 11))
+entry_x.pack(pady=5)
+entry_x.place(relx=0.03, rely=0.75)
+
+entry_y = ttk.Entry(root, width=9, background="white", font=("Lucida Sans Typewriter", 11))
+entry_y.pack(pady=5)
+entry_y.place(relx=0.14, rely=0.75)
 
 # Crea una etiqueta para el título "VALORES DE ENTRADA"
-valores_entrada_label = ttk.Label(root, text='VALORES DE ENTRADA', font=("Lucida Sans Typewriter", 20))
+valores_entrada_label = ttk.Label(root, text='VALORES DE ENTRADA', font=("Lucida Sans Typewriter", 20),background="white")
 valores_entrada_label.pack(pady=10)
 valores_entrada_label.place(relx=0.025, rely=0.025)
 # Crea una etiqueta para el título "RESULTADO"
-resultado_label = ttk.Label(root, text='RESULTADO',font=("Yu Gothic", 14))
+resultado_label = ttk.Label(root, text='RESULTADO',font=("Lucida Sans Typewriter", 20), background="white")
 resultado_label.pack(pady=10)
 resultado_label.place(relx=0.5, rely=0.1)
 #crea la etiqueta para el obstaculo
-obstaculo_label = ttk.Label(root, text='OBSTACULO',font=("Yu Gothic", 14))
+obstaculo_label = ttk.Label(root, text='OBSTACULO',font=("Lucida Sans Typewriter", 20), background="white")
 obstaculo_label.pack(pady=10)
-obstaculo_label.place(relx=0.5, rely=0.5)
+obstaculo_label.place(relx=0.07, rely=0.55)
+#crear etiquetas de x
+obstaculo_label = ttk.Label(root, text='X',font=("Lucida Sans Typewriter", 20), background="white")
+obstaculo_label.pack(pady=10)
+obstaculo_label.place(relx=0.050, rely=0.65)
+#crear etiqueta de y
+obstaculo_label = ttk.Label(root, text='Y',font=("Lucida Sans Typewriter", 20), background="white")
+obstaculo_label.pack(pady=10)
+obstaculo_label.place(relx=0.16, rely=0.65)
 
-# Crear los botones donde estaran los valores de entrada, donde si les picas, sale que son
-texto_masa = ttk.Label(root,text="masa (m)",font=("Lucida Sans Typewriter", 14))
+# Crear los entries donde estaran los valores de entrada,
+texto_masa = ttk.Label(root,text="Masa (m)",font=("Lucida Sans Typewriter", 14), background="white")
 texto_masa.place(relx=0.025, rely=0.1)
 
-texto_gravedad = ttk.Label(root,text="gravedad (g)", font=("Lucida Sans Typewriter", 14))
+texto_gravedad = ttk.Label(root,text="Gravedad (g)", font=("Lucida Sans Typewriter", 14), background="white")
 texto_gravedad.place(relx=0.025, rely=0.17)
 
-texto_constante = ttk.Label(root,text="constante del resorte (k)", font=("Lucida Sans Typewriter", 14))
+texto_constante = ttk.Label(root,text="Fuerza del resorte (k)", font=("Lucida Sans Typewriter", 14), background="white")
 texto_constante.place(relx=0.025, rely=0.24)
 
-texto_alturaInicial = ttk.Label(root,text="altura inicial (Ho)",font=("Lucida Sans Typewriter", 14))
+texto_alturaInicial = ttk.Label(root,text="Altura inicial (Ho)",font=("Lucida Sans Typewriter", 14), background="white")
 texto_alturaInicial.place(relx=0.025, rely=0.31)
 
-texto_alturaFinal = ttk.Label(root,text="altura final (Hf)", font=("Lucida Sans Typewriter", 14))
+texto_alturaFinal = ttk.Label(root,text="Altura final (Hf)", font=("Lucida Sans Typewriter", 14), background="white")
 texto_alturaFinal.place(relx=0.025, rely=0.38)
 
-texto_Longitud = ttk.Label(root,text="Longitud (L)", font=("Lucida Sans Typewriter", 14))
+texto_Longitud = ttk.Label(root,text="Longitud (L)", font=("Lucida Sans Typewriter", 14), background="white")
 texto_Longitud.place(relx=0.025, rely=0.45)
 
 
 # Crear el botón pa calcular
 calcular = ttk.Button(root, text="CALCULAR", style="Accent.TButton", command=calculo_tiro)
-calcular.place(relx=0.185, rely=0.51)
+calcular.place(relx=0.5, rely=0.55)
 
 #create the label for the result xc
-result_label = ttk.Label(root, text='')
-result_label.pack(pady=10)
-result_label.place(relx=0.5, rely=0.2)
+result_label = ttk.Label(root, text='compresión del resorte',background="red", relief="sunken", width=30,font=("Lucida Sans Typewriter", 14) )
+result_label.pack(pady=10,ipadx=10,ipady=10)
+result_label.place(relx=0.5, rely=0.2)  
 
 #create the label for the result angulo
-result_label2 = ttk.Label(root, text='',)
-result_label2.pack(pady=10)
+result_label2 = ttk.Label(root, text='',background="white",relief="sunken",width=30,font=("Lucida Sans Typewriter", 14))
+result_label2.pack(pady=10,ipadx=10,ipady=10)
 result_label2.place(relx=0.5, rely=0.4)
 
     #termina la parte de el layout de la calculadora
@@ -131,12 +148,19 @@ def siguiente_entry(event):
     elif event.widget == entry_Hf:
         entry_L.focus()
     elif event.widget == entry_L:
+        entry_x.focus()
+    elif event.widget == entry_x:
+        entry_y.focus()
         # Aquí puedes agregar más condicionales para mover el enfoque a más entradas si es necesario
         pass
 
 #FUNCION PARA HACER QUE LA FLECHA HACIA ARRIBA SIRVA PARA IR AL ENTRY ANTERIOR
 def entry_pasado(event):
-    if event.widget == entry_L:
+    if event.widget == entry_y:
+        entry_x.focus()
+    elif event.widget == entry_x:
+        entry_L.focus()
+    elif event.widget == entry_L:
         entry_Hf.focus()
     elif event.widget == entry_Hf:
         entry_Ho.focus()
@@ -157,8 +181,12 @@ entry_k.bind('<Down>', siguiente_entry)
 entry_Ho.bind('<Down>', siguiente_entry)
 entry_Hf.bind('<Down>', siguiente_entry)
 entry_L.bind('<Down>', siguiente_entry)
+entry_x.bind('<Down>', siguiente_entry)
+entry_y.bind('<Down>', siguiente_entry)
 
 #VINCULAR LA FLECHA HACIA ARRIBA PARA IR AL ENTRY DE ARRIBA
+entry_y.bind('<Up>', entry_pasado)
+entry_x.bind('<Up>', entry_pasado)
 entry_L.bind('<Up>', entry_pasado)
 entry_Hf.bind('<Up>', entry_pasado)
 entry_Ho.bind('<Up>', entry_pasado)
@@ -177,6 +205,8 @@ def borrar_campos(event=None):
     entry_Hf.delete(0, tk.END)
     entry_Ho.delete(0, tk.END)
     entry_L.delete(0, tk.END)
+    entry_x.delete(0, tk.END)
+    entry_y.delete(0, tk.END)
     entry_m.focus_set()
 
 # Crea el botón para borrar los campos
@@ -204,7 +234,7 @@ imagen = imagen.resize((250, 150), Image.ANTIALIAS)  # Cambia el tamaño de la i
 fondo = ImageTk.PhotoImage(imagen)
 # Crea la etiqueta y configura su posición y tamaño
 etiqueta = Label(root, image=fondo, width=250, height=150)
-etiqueta.place(x=70, y=440)
+etiqueta.place(x=1150, y=50)
 
     #Termina la parte de las imagenes en la calculadora
     #empieza la parte de la ayuda
